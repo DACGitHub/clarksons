fff146267a9cd0cb3091e045b46d931a88d20f79
Design and Code <32168776+DACGitHub@users.noreply.github.com>
2019-10-10 09:45:05 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,133 @@
+
+
+
+
+var section_one = document.getElementById("one");
+
+var animation_one = lottie.loadAnimation({
+container: section_one,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section01_v03.json"
+});
+
+
+var section_two = document.getElementById("two");
+
+var animation_two = lottie.loadAnimation({
+container: section_two,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section02_v01.json"
+});
+
+var section_three = document.getElementById("three");
+
+var animation_three = lottie.loadAnimation({
+container: section_three,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section03_v01.json"
+});
+
+//
+var section_four = document.getElementById("four");
+
+var animation_four = lottie.loadAnimation({
+container: section_four,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section04_v01.json"
+});
+
+//
+var section_five = document.getElementById("five");
+
+var animation_five = lottie.loadAnimation({
+container: section_five,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section05_v01.json"
+});
+
+//
+var section_six = document.getElementById("six");
+
+var animation_six = lottie.loadAnimation({
+container: section_six,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section06_v01.json"
+});
+
+//
+var section_seven = document.getElementById("seven");
+
+var animation_seven = lottie.loadAnimation({
+container: section_seven,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section07_v02.json"
+});
+
+//
+var section_eight = document.getElementById("eight");
+
+var animation_eight = lottie.loadAnimation({
+container: section_eight,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section08_v01.json"
+});
+
+//
+var section_nine = document.getElementById("nine");
+
+var animation_nine = lottie.loadAnimation({
+container: section_nine,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section09_v01.json"
+});
+
+//
+var section_ten = document.getElementById("ten");
+
+var animation_ten = lottie.loadAnimation({
+container: section_ten,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section10_v01.json"
+});
+
+//
+var section_eleven = document.getElementById("eleven");
+
+var animation_eleven = lottie.loadAnimation({
+container: section_eleven,
+renderer: "svg",
+loop: true,
+autoplay: true,
+prerender: true,
+path: "clarksons_section11_v01.json"
+});
